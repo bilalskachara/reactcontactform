e522228e0e54bf3caf5fb9de26d25231c813df46
Bilal Kachara <bilal.kachara@aimcrafters.com>
2021-06-04 16:05:53 +0530

    Updates

---
+++ precache-manifest.af0921579bfc150073aefe5aafa8fe7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6c183b1d934366fd050988d0085ee61",
+    "url": "/reactcontactform/index.html"
+  },
+  {
+    "revision": "44611aab10fa43382632",
+    "url": "/reactcontactform/static/css/2.8d3f7698.chunk.css"
+  },
+  {
+    "revision": "2a6e3e2139b26104b046",
+    "url": "/reactcontactform/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "44611aab10fa43382632",
+    "url": "/reactcontactform/static/js/2.5d2e2050.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/reactcontactform/static/js/2.5d2e2050.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a6e3e2139b26104b046",
+    "url": "/reactcontactform/static/js/main.ebdf3daa.chunk.js"
+  },
+  {
+    "revision": "fa682af07336d969866a",
+    "url": "/reactcontactform/static/js/runtime-main.4c0d1f65.js"
+  }
+]);
